c7061d13b029d761012f25405f385f7693d4a6c7
Jim Patt & ian bevel <student@epicodus.com>
2015-04-27 14:36:24 -0700

    adds second spec

--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -9,4 +9,11 @@ describe(:Delivery) do
       expect(test_package.volume()).to(eq("8"))
     end
   end
+
+  describe('#cost_to_ship') do
+    it('calculates the cost of shipment based on weight and volume') do
+      test_package = Delivery.new("2", "2", "2", "2")
+      expect(test_package.cost_to_ship()).to(eq("2.99"))
+    end
+  end
 end
